5a081b58db0dd66de1b362b6ca164091552c1605
mrgauz <github@gauz.net>
2021-10-25 21:47:21 +0200

    Little changes

--- app/src/main/java/com/example/noteapp/models/NotesLiveData.kt
+++ app/src/main/java/com/example/noteapp/models/NotesLiveData.kt
@@ -1,7 +1,6 @@
 package com.example.noteapp
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.noteapp.models.Note
 import com.google.firebase.database.DataSnapshot
@@ -9,9 +8,10 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
-class NotesLiveData : MutableLiveData<MutableList<Note>>(){
+class NotesLiveData : MutableLiveData<MutableList<Note>>() {
     private var reference: DatabaseReference
 
     init {
@@ -32,18 +32,21 @@ class NotesLiveData : MutableLiveData<MutableList<Note>>(){
             // because once data is changes onDataChange() from getNotes() will be called
         }
     }
+
     fun updateNote(note: Note) {
         if (note.uid != null) {
             // Update note under path /notes/$uid
             reference.child(note.uid!!).updateChildren(note.toMap())
         }
     }
+
     fun deleteNote(note: Note) {
         if (note.uid != null) {
             // Delete note under path /notes/$uid
             reference.child(note.uid!!).removeValue()
         }
     }
+
     fun getNotes(): NotesLiveData {
         // Called once when RecyclerView is initialized, after that all changes are handled
         // automatically by ValueEventListener
@@ -53,7 +56,7 @@ class NotesLiveData : MutableLiveData<MutableList<Note>>(){
                 var tmpNotesList = mutableListOf<Note>()
                 snapshot.children.forEach {
                     if (it != null) {
-                        val note = it.getValue(Note::class.java)!!
+                        val note = it.getValue<Note>()!!
                             .also { note -> note.uid = it.key.toString() }
                         tmpNotesList.add(note)
                     }
--- app/src/main/java/com/example/noteapp/models/NotesViewModel.kt
+++ app/src/main/java/com/example/noteapp/models/NotesViewModel.kt
@@ -1,29 +1,10 @@
 package com.example.noteapp.models
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.noteapp.NotesLiveData
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 
 class NotesViewModel : ViewModel() {
     private val notes = NotesLiveData()
-    private var reference: DatabaseReference
-
-    init {
-        // Initialize LiveData with empty list
-        notes.value = mutableListOf()
-        // Connect to database and create a reference to "notes" node
-        reference = Firebase
-            .database("https://kotlin-praktikum-default-rtdb.europe-west1.firebasedatabase.app/")
-            .getReference("notes")
-    }
 
     fun addNote(note: Note) {
         notes.addNote(note)
@@ -39,6 +20,5 @@ class NotesViewModel : ViewModel() {
 
     fun getNotes(): NotesLiveData {
         return notes.getNotes()
-
     }
 }
